689960496b8447e240184adbaa49cb1064fffe10
Darth Vader <omanlegend4@gmail.com>
2026-04-28 21:06:47 +0300

    feat: toggle follow state

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,14 +1,26 @@
+import React, { useState } from 'react';
 import profilepic from '../assets/profilepic.png';
 
 function Profile ({name, role}) {
+    const [isFollowing, setIsFollowing] = useState(false);
+
+    const handleFollow = () => {
+        setIsFollowing(!isFollowing);
+    };
+
   return (
     <div>
         <img src={profilepic} alt="Profile Picture" />
         <h1>{name}</h1>
         <p>{role}</p>
-        <button>Follow</button>
+        <button onClick={handleFollow}>
+            {isFollowing ? 'Following' : 'Follow'}
+        </button>
     </div>
-  );
+    
+  )
+    
 }
+
 
 export default Profile
